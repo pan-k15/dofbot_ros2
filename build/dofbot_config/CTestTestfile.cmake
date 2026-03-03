e2fc770498a0bdc1c48edf242e138921e42ee3d5
Pan <pan.techprototype@gmail.com>
2026-03-03 11:54:58 +0700

    add gripper controller

---
+++ build/dofbot_config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pan/Documents/projects/robot/p7/ros2_ws/src/dofbot_config
+# Build directory: /home/pan/Documents/projects/robot/p7/ros2_ws/src/build/dofbot_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/dofbot_config/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/pan/Documents/projects/robot/p7/ros2_ws/src/install/dofbot_moveit/share/dofbot_moveit/package.sh"
